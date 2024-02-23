6cec9463d38aebf184dd2fb28137cbc75b1e5570
Jack Hogan <jackhogan11@gmail.com>
2024-02-23 11:54:02 -0500

    Switched from snippy to luasnip

--- nvim/lua/plugins/LuaSnip.lua
+++ nvim/lua/plugins/LuaSnip.lua
@@ -3,11 +3,15 @@
 -- For detailed information on configuring this plugin, please refer to its
 -- official documentation:
 --
---   https://github.com/dcampos/nvim-snippy
+--   https://github.com/L3MON4D3/LuaSnip
 --
 -- If you wish to use this plugin, you can optionally modify and then uncomment
 -- the configuration below.
 
 return {
-  -- "dcampos/nvim-snippy"
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp"
 }
